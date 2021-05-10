3c5130dda5af81f9b18fa8f031efcbb4b2080d4b
meganedelux <meganedelux@gmail.com>
2021-05-10 16:50:04 +0900

    Initial Commit

---
+++ test02-Calert/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test02-Calert
+//
+//  Created by Kazuto Nakamura on 2021/05/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test02-Calert/test02_CalertApp.swift
@@ -0,0 +1,17 @@
+//
+//  test02_CalertApp.swift
+//  test02-Calert
+//
+//  Created by Kazuto Nakamura on 2021/05/10.
+//
+
+import SwiftUI
+
+@main
+struct test02_CalertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
